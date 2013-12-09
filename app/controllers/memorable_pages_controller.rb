56387f13a8416056dbf85b708969002b68d72f4f
Jeffrey Dill <jeffdill2@gmail.com>
2013-12-08 20:39:15 -0500

    Created the table for the Memorable page

--- app/controllers/memorable_pages_controller.rb
+++ app/controllers/memorable_pages_controller.rb
@@ -1,7 +1,11 @@
 class MemorablePagesController < ApplicationController
 
 	def index
+		@memories = MemorableInfo.all
+	end
 
+	def memory_params
+		params.require(:memory).permit(:subject, :description, :image, :date)
 	end
 	
 end
