95546a55331ab4c2476023a1e249d162fe90099f
Jeffrey Dill <jeffdill2@gmail.com>
2013-12-15 11:55:17 -0500

    Added Google Analytics tracking and pointed unique domain to Heroku

--- app/controllers/memorable_pages_controller.rb
+++ app/controllers/memorable_pages_controller.rb
@@ -1,6 +1,14 @@
+require "CGI"
+require "uri"
+
 class MemorablePagesController < ApplicationController
 
 	def index
+		incoming_url = request.referer
+		encoded_url = URI.encode(incoming_url)
+		#@incoming_url_params = CGI.parse(URI.parse(incoming_url).query)
+		@incoming_url_params = URI.parse(encoded_url)
+
 		@memories = MemorableInfo.all
 	end
 
