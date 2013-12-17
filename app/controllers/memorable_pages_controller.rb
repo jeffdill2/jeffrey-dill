b06d5c513193506bfe9271465baf49b96ba9c2f9
Jeffrey Dill <jeffdill2@gmail.com>
2013-12-16 20:56:54 -0500

    Fixed code - redeploying

--- app/controllers/memorable_pages_controller.rb
+++ app/controllers/memorable_pages_controller.rb
@@ -1,20 +1,12 @@
-require "CGI"
-require "uri"
-
 class MemorablePagesController < ApplicationController
 
 	def index
-<<<<<<< HEAD
-		incoming_url = request.referer
-		encoded_url = URI.encode(incoming_url)
+		#incoming_url = request.referer
+		#encoded_url = URI.encode(incoming_url)
 		#@incoming_url_params = CGI.parse(URI.parse(incoming_url).query)
-		@incoming_url_params = URI.parse(encoded_url)
+		#@incoming_url_params = URI.parse(encoded_url)
 
 		@memories = MemorableInfo.all
-	end
-=======
->>>>>>> parent of 56387f1... Created the table for the Memorable page
-
 	end
 	
 end
