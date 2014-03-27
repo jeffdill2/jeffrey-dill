d2120ab03c5091e03249ddc127f20ed644288bd2
Jeffrey Dill <jeffdill2@gmail.com>
2014-03-26 23:58:27 -0400

    Modified tweets helper to identify user names

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -2,7 +2,29 @@ module TweetsHelper
 	def tweet_text(tweet)
 		hshParsedTweet = JSON.parse(tweet.to_json)
 
-		hshParsedTweet['text']
+		strTweetParsedText = hshParsedTweet['text']
+		strTweetModifiedText = ""
+		strTweetUser = ""
+		bolUserFound = false
+
+		strTweetParsedText.each_char do |char|
+			strTweetCharacter = strTweetParsedText[char]
+
+			if (strTweetCharacter == "@" || bolUserFound) then
+				bolUserFound = true
+
+				if (strTweetCharacter == " ") then
+					bolUserFound = false
+					strTweetModifiedText = strTweetModifiedText + "<a href='http://www.twitter.com/" + strTweetUser[1..-1] + "' target='_blank'>" + strTweetUser + "</a> "
+				else
+					strTweetUser = strTweetUser + strTweetCharacter
+				end
+			else
+				strTweetModifiedText = strTweetModifiedText + strTweetCharacter
+			end
+		end
+
+		strTweetModifiedText.html_safe
 	end
 
 	def tweet_url(tweet)
