3425b7045ff205e2b353524a938cba0c85c6c486
Jeffrey Dill <jeffdill2@gmail.com>
2014-04-03 13:11:35 -0400

    Improved formatting and content on professional page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140316012719) do
+ActiveRecord::Schema.define(version: 20140403033511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,12 @@ ActiveRecord::Schema.define(version: 20140316012719) do
     t.text     "description"
     t.string   "image"
     t.date     "date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "skills", force: true do |t|
+    t.string   "skill"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
